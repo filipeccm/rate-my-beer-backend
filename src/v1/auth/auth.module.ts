36d77a8ec1c05ca21df410afb3904e1fff7e125c
filipe <filipeccm@gmail.com>
2024-07-22 15:10:03 +0200

    remove PrismaService module imports as it is globally imported

--- src/v1/auth/auth.module.ts
+++ src/v1/auth/auth.module.ts
@@ -3,11 +3,10 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { JWTStrategy } from './strategy/jwt.strategy';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, JWTStrategy],
+  providers: [AuthService, JWTStrategy],
 })
 export class AuthModule {}
--- src/v1/beers/beers.module.ts
+++ src/v1/beers/beers.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BeersService } from './beers.service';
 import { BeersController } from './beers.controller';
-import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [BeersController],
-  providers: [BeersService, PrismaService],
+  providers: [BeersService],
 })
 export class BeersModule {}
