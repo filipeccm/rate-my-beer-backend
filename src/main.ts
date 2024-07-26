9a57a8335dfefe21bfacc5f1b01fc2059a6a0d09
filipe <filipeccm@gmail.com>
2024-07-26 14:20:20 +0200

    implement express helmet and increase request body size limit

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './v1/app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/v1');
+  app.use(json({ limit: '10mb' }));
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle('Rate My Beer API')
