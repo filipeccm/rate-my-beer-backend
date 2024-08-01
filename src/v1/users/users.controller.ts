37c15dbe893804cff335719a8f626b95138ea5ea
filipe <filipeccm@gmail.com>
2024-08-01 18:23:55 +0200

    create new endpoints for user's liked and rated beers

--- src/v1/users/users.controller.ts
+++ src/v1/users/users.controller.ts
@@ -1,24 +1,89 @@
-import { Controller, Get, Body, Patch, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Delete,
+  Body,
+  Patch,
+  UseGuards,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from '../auth/decorators/get-user.decorator';
 import { ApiExtraModels } from '@nestjs/swagger';
 import { UserEntity } from './entities/user.entity';
+import { LikeBeerDto } from './dto/like-beer.dto';
+import { RateBeerDto } from './dto/rate-beer.dto';
 
 @ApiExtraModels(UserEntity)
-@UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(AuthGuard('jwt'))
+  @Patch()
+  update(@GetUser('id') id: number, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
   @Get('me')
   getMe(@GetUser('id') id: number) {
     return this.usersService.getMe(id);
   }
 
-  @Patch()
-  update(@GetUser('id') id: number, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(id, updateUserDto);
+  @Get(':id/liked-beers')
+  getUserLikedBeers(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserLikedBeers(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post(':id/liked-beers')
+  likeBeer(
+    @GetUser('id') jwtUserId: number,
+    @Param('id', ParseIntPipe) userId: number,
+    @Body() dto: LikeBeerDto,
+  ) {
+    return this.usersService.likeBeer(jwtUserId, userId, dto.beerId);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id/liked-beers/:beerId')
+  dislikeBeer(
+    @GetUser('id') jwtUserId: number,
+    @Param('id', ParseIntPipe) userId: number,
+    @Param('beerId', ParseIntPipe) beerId: number,
+  ) {
+    return this.usersService.dislikeBeer(jwtUserId, userId, beerId);
+  }
+
+  @Get(':id/rated-beers')
+  getUserRatedBeers(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.getUserRatedBeers(id);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Put(':id/rated-beers/:beerId')
+  rateBeer(
+    @GetUser('id') jwtUserId: number,
+    @Param('id', ParseIntPipe) userId: number,
+    @Param('beerId', ParseIntPipe) beerId: number,
+    @Body() dto: RateBeerDto,
+  ) {
+    return this.usersService.rateBeer(jwtUserId, userId, beerId, dto);
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Delete(':id/rated-beers/:beerId')
+  unrateBeer(
+    @GetUser('id') jwtUserId: number,
+    @Param('id', ParseIntPipe) userId: number,
+    @Param('beerId', ParseIntPipe) beerId: number,
+  ) {
+    return this.usersService.unrateBeer(jwtUserId, userId, beerId);
   }
 }
--- src/v1/users/users.service.ts
+++ src/v1/users/users.service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { RateBeerDto } from './dto/rate-beer.dto';
 
 @Injectable()
 export class UsersService {
@@ -34,5 +41,146 @@ export class UsersService {
     });
 
     return user;
+  }
+
+  async getUserLikedBeers(id: number) {
+    const likedBeers = await this.prisma.usersBeersLikes.findMany({
+      where: { userId: id },
+      select: {
+        beer: true,
+      },
+    });
+
+    if (!likedBeers.length) {
+      const user = await this.prisma.user.findUnique({ where: { id } });
+      if (!user) {
+        throw new NotFoundException('User does not exist');
+      }
+      return [];
+    }
+
+    return {
+      total: likedBeers.length,
+      liked_beers: likedBeers.map((likedBeer) => likedBeer.beer),
+    };
+  }
+
+  async likeBeer(jwtUserId: number, userId: number, beerId: number) {
+    if (jwtUserId !== userId) {
+      throw new UnauthorizedException();
+    }
+    try {
+      return await this.prisma.usersBeersLikes.create({
+        data: {
+          userId,
+          beerId,
+        },
+      });
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2002')
+          throw new BadRequestException('User has already liked beer');
+        if (err.code === 'P2003') throw new BadRequestException();
+      }
+      throw new Error();
+    }
+  }
+
+  async dislikeBeer(jwtUserId: number, userId: number, beerId: number) {
+    if (jwtUserId !== userId) {
+      throw new UnauthorizedException();
+    }
+    try {
+      return await this.prisma.usersBeersLikes.delete({
+        where: {
+          userId_beerId: {
+            userId,
+            beerId,
+          },
+        },
+      });
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2025')
+          throw new BadRequestException('Record does not exist');
+      }
+      throw new Error();
+    }
+  }
+
+  async getUserRatedBeers(id: number) {
+    const ratedBeers = await this.prisma.usersBeersRatings.findMany({
+      where: { userId: id },
+      select: {
+        beer: true,
+      },
+    });
+
+    if (!ratedBeers.length) {
+      const user = await this.prisma.user.findUnique({ where: { id } });
+      if (!user) {
+        throw new NotFoundException('User does not exist');
+      }
+      return [];
+    }
+
+    return {
+      total: ratedBeers.length,
+      rated_beers: ratedBeers.map((b) => b.beer),
+    };
+  }
+
+  async rateBeer(
+    jwtUserId: number,
+    userId: number,
+    beerId: number,
+    dto: RateBeerDto,
+  ) {
+    if (jwtUserId !== userId) {
+      throw new UnauthorizedException();
+    }
+    try {
+      return await this.prisma.usersBeersRatings.upsert({
+        where: {
+          userId_beerId: {
+            userId,
+            beerId,
+          },
+        },
+        update: { rating: dto.rating },
+        create: { userId, beerId, rating: dto.rating },
+        select: {
+          rating: true,
+        },
+      });
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2003') throw new BadRequestException();
+        if (err.code === 'P2025')
+          throw new BadRequestException('Record does not exist');
+      }
+    }
+  }
+
+  async unrateBeer(jwtUserId: number, userId: number, beerId: number) {
+    if (jwtUserId !== userId) {
+      throw new UnauthorizedException();
+    }
+    try {
+      await this.prisma.usersBeersRatings.delete({
+        where: {
+          userId_beerId: {
+            userId,
+            beerId,
+          },
+        },
+      });
+
+      return { message: 'Rating has been deleted', data: { rated: false } };
+    } catch (err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+        if (err.code === 'P2025') throw new BadRequestException();
+      }
+    }
   }
 }
